593d50432bd9a424c58b5bb8841aa590ffa3159a
LHerdy <le.herdy@gmail.com>
2020-09-18 05:54:00 -0300

    Organizing the code.

--- api_basic/views.py
+++ api_basic/views.py
@@ -21,6 +21,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
+
 # class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
 #                                               mixins.CreateModelMixin,
 #                                               mixins.CreateModelMixin,
@@ -35,26 +36,26 @@ class ArticleViewSet(viewsets.ModelViewSet):
 #         articles = Article.objects.all()
 #         serializer = ArticleSerializer(articles, many=True)
 #         return Response(serializer.data)
+
 #     def create(self, request):
 #         serializer = ArticleSerializer(data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 #     def retrieve(self, request, pk=None):
 #         queryset = Article.objects.all()
 #         article = get_object_or_404(queryset, pk=pk)
 #         serializer = ArticleSerializer(article)
 #         return Response(serializer.data)
+
 #     def update(self, request, pk=None):
 #         article = Article.objects.get(pk=pk)
 #         serializer = ArticleSerializer(article, data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data)
-
-
-
 
 
 class GenericAPIView(generics.GenericAPIView,
@@ -76,16 +77,15 @@ class GenericAPIView(generics.GenericAPIView,
             return self.retrieve(request)
         else:
             return self.list(request)
+
     def post(self, request):
         return self.create(request)
+
     def put(self, request, id=None):
         return self.update(request, id)
+
     def delete(self, request, id):
         return self.destroy(request, id)
-
-
-
-
 
 
 class ArticleAPIView(APIView):
@@ -93,6 +93,7 @@ class ArticleAPIView(APIView):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
+
     def post(self, request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
@@ -106,26 +107,23 @@ class ArticleDetail(APIView):
             return Article.objects.get(id=id)
         except Article.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
     def get(self, request, id):
         article = self.get_object(id)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+
     def put(self, request, id):
         article = self.get_object(id)
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+
     def delete(self, request, id):
         article = self.get_object(id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
-
 
 
 @api_view(['GET', 'POST'])
@@ -140,7 +138,6 @@ def article_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
